50f56c2f85f79e7a59a78f1818709122440ca3cb
Davi Alves Maia <maiadavi3266@gmail.com>
2025-07-25 20:12:12 -0300

    adição de matrizes

--- matriz.asm
+++ matriz.asm
@@ -1,66 +1,135 @@
-.section .data
-matriz1:    .word 1, 2, 3, 4      @ Primeira matriz 2x2 (elementos em ordem linear)
-matriz2:    .word 5, 6, 7, 8      @ Segunda matriz 2x2 (elementos em ordem linear)
-msg1:       .asciz "Matriz 1:\n"  @ Mensagem para exibir antes da matriz 1
-msg2:       .asciz "Matriz 2:\n"  @ Mensagem para exibir antes da matriz 2
-format:     .asciz "%d %d\n"      @ Formato para imprimir duas colunas
+section .data
+    ; Matrizes A e B (2x2)
+    A dd 1, 2
+      dd 3, 4
 
-.section .text
-.global main
-main:
-    @ Exibe "Matriz 1:"
-    ldr r0, =msg1                 @ Carrega endereço da mensagem msg1 em r0
-    bl printf                     @ Chama printf para exibir msg1
+    B dd 5, 6
+      dd 7, 8
 
-    @ Exibe primeira linha da matriz 1
-    ldr r1, =matriz1              @ Carrega endereço da matriz1 em r1
-    ldr r2, [r1]                  @ Carrega elemento [0] (linha 1, coluna 1) em r2
-    ldr r3, [r1, #4]              @ Carrega elemento [1] (linha 1, coluna 2) em r3
-    ldr r0, =format               @ Carrega endereço do formato em r0
-    mov r4, r2                    @ Move valor de r2 para r4 (primeiro argumento)
-    mov r5, r3                    @ Move valor de r3 para r5 (segundo argumento)
-    mov r1, r4                    @ Prepara r1 para printf (primeiro valor a imprimir)
-    mov r2, r5                    @ Prepara r2 para printf (segundo valor a imprimir)
-    bl printf                     @ Chama printf para exibir a primeira linha
+    ; Mensagem de cabeçalho
+    msg db "Resultado da soma das matrizes:", 0xA, 0
+    msg_len equ $ - msg
 
-    @ Exibe segunda linha da matriz 1
-    ldr r2, [r1, #8]              @ Carrega elemento [2] (linha 2, coluna 1) em r2
-    ldr r3, [r1, #12]             @ Carrega elemento [3] (linha 2, coluna 2) em r3
-    ldr r0, =format               @ Carrega endereço do formato em r0
-    mov r4, r2                    @ Move valor de r2 para r4
-    mov r5, r3                    @ Move valor de r3 para r5
-    mov r1, r4                    @ Prepara r1 para printf
-    mov r2, r5                    @ Prepara r2 para printf
-    bl printf                     @ Chama printf para exibir a segunda linha
+section .bss
+    ; Matriz Resultante R
+    R resd 4
 
-    @ Exibe "Matriz 2:"
-    ldr r0, =msg2                 @ Carrega endereço da mensagem msg2 em r0
-    bl printf                     @ Chama printf para exibir msg2
+    ; Buffer para conversão de número em string (máx 10 dígitos + newline + null)
+    num_str resb 12
 
-    @ Exibe primeira linha da matriz 2
-    ldr r1, =matriz2              @ Carrega endereço da matriz2 em r1
-    ldr r2, [r1]                  @ Carrega elemento [0] (linha 1, coluna 1) em r2
-    ldr r3, [r1, #4]              @ Carrega elemento [1] (linha 1, coluna 2) em r3
-    ldr r0, =format               @ Carrega endereço do formato em r0
-    mov r4, r2                    @ Move valor de r2 para r4
-    mov r5, r3                    @ Move valor de r3 para r5
-    mov r1, r4                    @ Prepara r1 para printf
-    mov r2, r5                    @ Prepara r2 para printf
-    bl printf                     @ Chama printf para exibir a primeira linha
+section .text
+    global _start
 
-    @ Exibe segunda linha da matriz 2
-    ldr r2, [r1, #8]              @ Carrega elemento [2] (linha 2, coluna 1) em r2
-    ldr r3, [r1, #12]             @ Carrega elemento [3] (linha 2, coluna 2) em r3
-    ldr r0, =format               @ Carrega endereço do formato em r0
-    mov r4, r2                    @ Move valor de r2 para r4
-    mov r5, r3                    @ Move valor de r3 para r5
-    mov r1, r4                    @ Prepara r1 para printf
-    mov r2, r5                    @ Prepara r2 para printf
-    bl printf                     @ Chama printf para exibir a segunda linha
+_start:
+    ; -------------------
+    ; Soma as matrizes
+    ; -------------------
+    mov esi, A
+    mov edi, B
+    mov ebx, R
+    mov ecx, 4
 
-    @ Finaliza o programa
-    mov r7, #1                    @ Código da syscall exit
-    mov r0, #0                    @ Código de saída 0
-    svc #0                        @ Chama o kernel para encerrar o programa
+soma_loop:
+    mov eax, [esi]
+    add eax, [edi]
+    mov [ebx], eax
 
-.extern printf                    @ Declara printf como função externa
+    add esi, 4
+    add edi, 4
+    add ebx, 4
+
+    loop soma_loop
+
+    ; -------------------
+    ; Escreve cabeçalho
+    ; -------------------
+    mov eax, 4
+    mov ebx, 1
+    mov ecx, msg
+    mov edx, msg_len
+    int 0x80
+
+    ; -------------------
+    ; Escreve os 4 valores da matriz R
+    ; -------------------
+    mov esi, R        ; ponteiro para matriz R
+    mov ecx, 4        ; número de elementos
+
+print_loop:
+    mov eax, [esi]    ; carrega valor
+    call int_to_str   ; converte para string em num_str
+
+    ; syscall write para mostrar string
+    mov eax, 4
+    mov ebx, 1
+    mov ecx, num_str
+    mov edx, eax      ; tamanho da string foi retornado em EAX pela função
+    int 0x80
+
+    add esi, 4
+    loop print_loop
+
+    ; fim do programa - exit(0)
+    mov eax, 1
+    xor ebx, ebx
+    int 0x80
+
+; -------------------------------
+; Função: int_to_str
+; Entrada: EAX = número inteiro
+; Saída: converte número para string em num_str
+; Retorna em EAX o comprimento da string (com \n)
+; -------------------------------
+int_to_str:
+    push eax
+    push ebx
+    push ecx
+    push edx
+
+    mov ecx, 10         ; base decimal
+    mov edi, num_str+10 ; final do buffer
+    mov byte [edi], 0xA ; newline
+
+    dec edi
+
+    cmp eax, 0
+    jnz .convert
+
+    mov byte [edi], '0'
+    dec edi
+    jmp .done
+
+.convert:
+    xor edx, edx
+
+.next_digit:
+    div ecx
+    add dl, '0'
+    mov [edi], dl
+    dec edi
+    xor edx, edx
+    cmp eax, 0
+    jne .next_digit
+
+.done:
+    inc edi
+    mov esi, edi
+    mov ecx, num_str
+
+.copy_loop:
+    mov al, [esi]
+    mov [ecx], al
+    inc esi
+    inc ecx
+    cmp al, 0xA
+    jne .copy_loop
+
+    mov eax, ecx
+    sub eax, num_str    ; retorna comprimento da string em EAX
+
+    pop edx
+    pop ecx
+    pop ebx
+    pop eax
+    ret
+; Fim do programa
